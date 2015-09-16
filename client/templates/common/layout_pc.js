406986f185d817123a7c0ae87872fed637e2f605
Ciro Lv <ciro.lv@fedeen.com>
2015-09-16 11:42:45 +0800

    empty folder intros

--- client/templates/common/layout_pc.js
+++ client/templates/common/layout_pc.js
@@ -2,3 +2,11 @@
  * Created by Ciro on 15/9/14.
  */
 
+Template.layoutPc.rendered = function(){
+    $(function(){
+        //服务端的ajax调用
+        //$.post("/test", {a:1},function(data){
+        //    console.log(data);
+        //});
+    });
+};
--- client/templates/main.js
+++ client/templates/main.js
@@ -3,3 +3,11 @@
  Created By Ciro
  */
 
+//Handlebars注册全局Helper标签(全局模板变量判断客户端类型)
+Handlebars.registerHelper('isMobile', function(){
+    if( _deviceType.android || _deviceType.iPhone || _deviceType.iPad){
+        return true;
+    }else{
+        return false;
+    }
+});
