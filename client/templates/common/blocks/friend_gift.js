8c475e3aeeefbbed729fc3bba73b81d6dfcc72a4
Jenny Yi <jenny.yi@fedeen.com>
2015-09-22 17:57:00 +0800

    facebook链接进入领取激活码第二种情况页面制作

---
+++ client/templates/common/blocks/friend_gift.js
@@ -0,0 +1,13 @@
+Template.FriendGift.rendered = function() {
+	$(function(){
+    //滚动框效果
+		 var swiper = new Swiper('.swiper-container', {
+        scrollbar: '.swiper-scrollbar',
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        mousewheelControl: true,
+        freeMode: true,
+        scrollbarHide:false
+    });
+});
+}
